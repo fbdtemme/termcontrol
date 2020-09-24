66a200226a8f03b1a320ac3d2f6419b520714b4d
fbdtemme <floriandetemmerman@gmail.com>
2020-09-24 16:42:10 +0200

    remove build dir and add gitignore

--- build/tests/termcontrol-tests_include-b858cb2.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/fbdtemme/Documents/project/torrent/termcontrol/build/tests/termcontrol-tests_tests-b858cb2.cmake")
-  include("/home/fbdtemme/Documents/project/torrent/termcontrol/build/tests/termcontrol-tests_tests-b858cb2.cmake")
-else()
-  add_test(termcontrol-tests_NOT_BUILT-b858cb2 termcontrol-tests_NOT_BUILT-b858cb2)
-endif()
