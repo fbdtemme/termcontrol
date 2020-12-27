7f25cecb25a7fed48e5208d683a4058a8746c4dc
fbdtemme <floriandetemmerman@gmail.com>
2020-12-27 16:49:00 +0100

    Remove build dir

--- cmake-build-debug/tests/termcontrol-tests_include-b858cb2.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/fbdtemme/Documents/project/torrent/termcontrol/cmake-build-debug/tests/termcontrol-tests_tests-b858cb2.cmake")
-  include("/home/fbdtemme/Documents/project/torrent/termcontrol/cmake-build-debug/tests/termcontrol-tests_tests-b858cb2.cmake")
-else()
-  add_test(termcontrol-tests_NOT_BUILT-b858cb2 termcontrol-tests_NOT_BUILT-b858cb2)
-endif()
