3ec1547c92f8b332519a00a49ad350f9feb6f8c1
fbdtemme <floriandetemmerman@gmail.com>
2021-01-14 16:49:42 +0100

    Allow seperate download of dependencies

--- external/external.cmake
+++ external/external.cmake
@@ -6,6 +6,10 @@ include(GNUInstallDirs)
 find_package(ctre QUIET)
 if (ctre_FOUND OR TARGET ctre::ctre)
     message(STATUS "Local installation of dependency ctre found.")
+elseif(EXISTS ${CMAKE_CURRENT_LIST_DIR}/ctre)
+    message(STATUS "ctre source directory found")
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/ctre)
+    set(ctre_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/ctre)
 else()
     message(STATUS "Fetching dependency ctre ...")
     FetchContent_Declare(
@@ -19,9 +23,18 @@ else()
     FetchContent_MakeAvailable(ctre)
 endif()
 
+if(IS_DIRECTORY "${ctre_SOURCE_DIR}")
+set_property(DIRECTORY ${ctre_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL YES)
+endif()
+
+
 find_package(gsl-lite QUIET)
 if (gsl-lite_FOUND OR TARGET gsl::gsl-lite-v1)
     message(STATUS "Local installation of gsl-lite found.")
+elseif(EXISTS ${CMAKE_CURRENT_LIST_DIR}/gsl-lite)
+    message(STATUS "gsl-lite source directory found")
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/gsl-lite)
+    set(gsl-lite_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/gsl-lite)
 else()
     message(STATUS "Fetching dependency gsl-lite...")
     FetchContent_Declare(
@@ -32,10 +45,18 @@ else()
     FetchContent_MakeAvailable(gsl-lite)
 endif()
 
+if(IS_DIRECTORY "${gsl-lite_SOURCE_DIR}")
+    set_property(DIRECTORY ${gsl-lite_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL YES)
+endif()
 
-find_package(fmt QUIET )
-if (fmt_FOUND OR TARGET fmt::fmt)
+
+find_package(fmt QUIET)
+if (fmt_FOUND OR TARGET fmt::fmt-header-only)
     message(STATUS "Local installation of dependency fmt found.")
+elseif(EXISTS ${CMAKE_CURRENT_LIST_DIR}/fmt)
+    message(STATUS "fmt source directory found")
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/fmt)
+    set(fmt_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/fmt)
 else()
     message(STATUS "Fetching dependency fmt...")
     FetchContent_Declare(
@@ -48,17 +69,32 @@ else()
     FetchContent_MakeAvailable(fmt)
 endif()
 
+if(IS_DIRECTORY "${fmt_SOURCE_DIR}")
+    set_property(DIRECTORY ${gsl-lite_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL YES)
+endif()
 
-find_package(Catch2 QUIET)
-if (Catch2_FOUND OR TARGET Catch2::Catch2)
-    message(STATUS "Local installation of Catch2 found.")
-else()
-    message(STATUS "Fetching dependency Catch2...")
-    FetchContent_Declare(
-            Catch2
-            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-            GIT_TAG        v2.x
-    )
-    FetchContent_MakeAvailable(Catch2)
+
+if (TERMCONTROL_BUILD_TESTS)
+    find_package(Catch2 QUIET)
+    if (Catch2_FOUND OR TARGET Catch2::Catch2)
+        message(STATUS "Local installation of Catch2 found.")
+    elseif(EXISTS ${CMAKE_CURRENT_LIST_DIR}/Catch2)
+        message(STATUS "Catch2 source directory found")
+        add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/Catch2)
+        set(Catch2_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/Catch2)
+    else()
+        message(STATUS "Fetching dependency Catch2...")
+        FetchContent_Declare(
+                Catch2
+                GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+                GIT_TAG        v2.x
+        )
+        FetchContent_MakeAvailable(Catch2)
+    endif()
+
     list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/contrib")
+
+    if(IS_DIRECTORY "${Catch2_SOURCE_DIR}")
+        set_property(DIRECTORY ${Catch2_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL YES)
+    endif()
 endif()
