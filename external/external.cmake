9a888bfe513b0bf126abe2216c1230c489277bdc
fbdtemme <floriandetemmerman@gmail.com>
2021-01-14 15:25:00 +0100

    Add missing catch2 dependency

--- external/external.cmake
+++ external/external.cmake
@@ -64,3 +64,18 @@ else()
     set(BUILD_SHARED_LIBS ON)
     FetchContent_MakeAvailable(fmt)
 endif()
+
+
+find_package(Catch2 QUIET)
+if (Catch2_FOUND OR TARGET Catch2::Catch2)
+    message(STATUS "Local installation of Catch2 found.")
+else()
+    message(STATUS "Fetching dependency Catch2...")
+    FetchContent_Declare(
+            Catch2
+            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+            GIT_TAG        v2.x
+    )
+    FetchContent_MakeAvailable(Catch2)
+    list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/contrib")
+endif()
