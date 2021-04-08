690c1c53761174b5225ca9ebb73e55683baada2b
fbdtemme <floriandetemmerman@gmail.com>
2021-04-08 12:07:18 +0200

    Add function to strip control sequences from strings

--- include/termcontrol/detail/parse.hpp
+++ include/termcontrol/detail/parse.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <ctre.hpp>
 #include <ctll.hpp>
+#include <algorithm>
 
 inline constexpr auto escape_seq_regex = ctll::fixed_string {
         "(?:" "\x1B" "[@-Z\\-_]|[" "\x80" "-" "\x9A" "\x9C" "-" "\x9F" "]"
@@ -20,23 +21,43 @@ constexpr auto total_control_sequences_length(std::string_view text) -> std::siz
     }
     return size;
 }
-
-///// Control
-//template <typename ForwardIt>
-//constexpr void search_control_sequences(ForwardIt first, ForwardIt last, OutputIt out) -> std::size_t
-//{
-//    string_view
-//    std::size_t pos = 0;
-//    auto match = ctre::search<escape_seq_regex>(first, last);
 //
-//    while (match) {
-//        auto m = match.get<0>();
-//        pos += std::distance(first, m.begin());
-//        *out++ = std::pair(pos, pos + m.size());
-//        match = ctre::search<escape_seq_regex>(m.end(), last);
-//    }
-//    return;
-//}
+
+inline std::string strip_control_sequences(std::string_view text)
+{
+    std::string out;
+    auto inserter = std::back_inserter(out);
+
+    auto match = ctre::search<escape_seq_regex>(text.begin(), text.end());
+    auto prev = text.begin();
+
+    while (match) {
+        auto m = match.get<0>();
+        std::copy(prev, m.begin(), inserter);
+        match = ctre::search<escape_seq_regex>(m.end(), text.end());
+        prev = m.end();
+    }
+    std::copy(prev, text.end(), inserter);
+    return out;
+}
+
+inline std::wstring strip_control_sequences(std::wstring_view text)
+{
+    std::wstring out;
+    auto inserter = std::back_inserter(out);
+
+    auto match = ctre::search<escape_seq_regex>(text.begin(), text.end());
+    auto prev = text.begin();
+
+    while (match) {
+        auto m = match.get<0>();
+        std::copy(prev, m.begin(), inserter);
+        match = ctre::search<escape_seq_regex>(m.end(), text.end());
+        prev = m.end();
+    }
+    std::copy(prev, text.end(), inserter);
+    return out;
+}
 //
 //
 
--- tests/test_parse.cpp
+++ tests/test_parse.cpp
@@ -11,3 +11,9 @@ TEST_CASE("test parse")
     auto s = termcontrol::total_control_sequences_length("Test\033[2m]BlahBlah\033[1AEnd");
     CHECK(s == 8);
 }
+
+TEST_CASE("test strip")
+{
+    auto s = termcontrol::strip_control_sequences("Test\033[2m]BlahBlah\033[1AEnd");
+    CHECK(s == "Test]BlahBlahEnd");
+}
